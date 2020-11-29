d643854e85e873a0bfd6a09e4ded1a1abca54cf1
nv-systems <nicolas.viallard@gmail.com>
2020-11-29 15:44:29 +0100

    reset

--- packages/standard/templates/snippets/related_projects.php
+++
@@ -1,6 +0,0 @@
-<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-<@~ newPagelist { type: 'related', sort: @{ sortRelatedPages | def ('date desc') } } @>	
-<@~ if @{ :pagelistCount } @>
-	<@ related.php @>
-	<@ ../blocks/pagelist/portfolio.php @>
-<@ end ~@>
